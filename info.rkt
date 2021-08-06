2b1cacd250d5eb0751e161d48c31e1bdfbe9f15f
Jeffrey Massung <massung@gmail.com>
2021-08-06 12:04:23 -0500

    Updated deps.

--- info.rkt
+++ info.rkt
@@ -4,6 +4,6 @@
 (define version "0.1.0")
 (define pkg-authors '("massung@gmail.com"))
 (define pkg-desc "A fast, efficient, immutable, dataframes implementation")
-(define deps '("base"))
+(define deps '("base" "racket-doc" "scribble-doc"))
 (define scribblings '(("scribblings/tabular-asa.scrbl" ())))
 (define compile-omit-paths '("test"))
